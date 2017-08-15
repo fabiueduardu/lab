12f18a2dae5e5143d0fe854817ce64443d79d394
Fabio Eduardo <fabiueduardu@hotmail.com>
2017-08-15 08:08:47 -0300

    refresh favorite

--- src/jquery.favorite/jquery.favorite.js
+++ src/jquery.favorite/jquery.favorite.js
@@ -1,82 +1,113 @@
-(function( $ ) {
-    var plugin_name ='favorite';
+(function($) {
+    var plugin_name = 'favorite';
 
-$.fn[plugin_name] = function(options) {
-    var self = this;
-    var settings = $.extend({
-        selector: this.selector,
-        serviceUrlGet: null,
-        serviceUrlPost: null,
-        serviceUrlPostRemove: null,
-        startGet: true,
-        classFav: 'active',
-        classNoFav: 'inactive'
-    }, options );
-    
-    /*## METHODS ## */
-    self.debug = function(data){
-        var message = 'debug: ' + plugin_name + ': ' + JSON.stringify(data);
-        alert(message);
-        console.log(message);
-    }
+    $.fn[plugin_name] = function(selector, options) {
+        var self = this;
 
-    self.ajaxerror = function(data){
-        self.debug(data);
-    }
+        var settings = $.extend({
+            selector: selector,
+            serviceUrlGet: null,
+            serviceUrlPost: null,
+            serviceUrlPostRemove: null,
+            startGet: true,
+            classFav: 'active',
+            classNoFav: 'inactive',
+            data_already: 'already',
+            onNotAuthenticated: null,
+            onNotAuthenticatedAttrAdd: 'data-login-required',
+            onNotAuthenticatedResultValue: 'NotAuthenticated',
+        }, options);
 
-    self.getSelectorName = function(){
-        var value = settings.selector.substring(6);
-        return value.substring(0,value.length-1);   
-    }
+        /*## METHODS ## */
+        self.debug = function(data) {
+            var message = 'debug: ' + plugin_name + ': ' + JSON.stringify(data);
+            console.log(message);
+        }
 
-    self.doClassFav = function(item){
-        var like = item.Like=='true';
-        var $target = $('[data-'+this.getSelectorName() +'="'+item.Id+'"]');
-    
-        $target.removeClass(settings.classFav);
-        $target.removeClass(settings.classNoFav);
-        $target.addClass(like ? settings.classFav : settings.classNoFav).text(item.Total);
-    }
+        self.ajaxerror = function(data) {
+            self.debug(data);
+        }
 
-    self.doGet = function(){
-      var serviceUrl = settings.serviceUrlGet;
-      var id_collection = [];
+        self.doOnAuthenticated = function(item, callback) {
+            if (item.ResultValue == settings.onNotAuthenticatedResultValue) {
+                if (settings.onNotAuthenticated)
+                    eval(settings.onNotAuthenticated)(item);
+            } else if (callback)
+                callback(item);
+        }
 
-      $(settings.selector).each(function(){
-        var $target = $(this);
-        var id =  $target.data(self.getSelectorName());
-        id_collection.push(id);
-      });     
-   
-     $.getJSON(serviceUrl,{ids: id_collection})
-            .done(function(data){$(data).each(function(i, item){ self.doClassFav(item); });})
-            .error(self.ajaxerror);
-    }
+        self.getSelectorName = function() {
+            var value = settings.selector.substring(6);
+            return value.substring(0, value.length - 1);
+        }
 
-    self.onClick  = function($target) 
-    {
-        var serviceUrl = settings.serviceUrlPost;
-        var id =  $target.data(self.getSelectorName());
+        self.doClassFav = function(item) {
+            var like = item.Like == true || item.Like == 'true';
+            var $target = $('[data-' + self.getSelectorName() + '="' + item.Id + '"]');
 
-        if($target.hasClass(settings.classFav))
-            serviceUrl = settings.serviceUrlPostRemove;
+            $target.removeClass(settings.classFav)
+                .removeClass(settings.classNoFav)
+                .addClass(like ? settings.classFav : settings.classNoFav).text(item.Total)
+                .css({ cursor: "pointer" });
 
-    $.post(serviceUrl , {id: id},null, 'json')
-        .done(function(data)
-        {
-            $(data).each(function(i, item){
-                if(id==item.Id) self.doClassFav(item); 
+            if (item.ResultValue == settings.onNotAuthenticatedResultValue) {
+                $target.attr(settings.onNotAuthenticatedAttrAdd, true).css({ cursor: "default" });
+            }
+        }
+
+        self.doGet = function() {
+            var serviceUrl = settings.serviceUrlGet;
+            var id_collection = [];
+
+            $(settings.selector).each(function() {
+                var $target = $(this);
+                var id = $target.data(self.getSelectorName());
+
+                var value_attr_already = self.getSelectorName() + '-' + settings.data_already;
+                if (!$target.data(value_attr_already))
+                    id_collection.push(id);
+
+                $target.attr('data-' + value_attr_already, true);
             });
-        }).error(self.ajaxerror);  
-    }
 
-    /*## INIT ## */
-    $(document).on('click',this.selector,function(){self.onClick( $(this));});
+            if (id_collection.length == 0)
+                return;
 
-    if(settings.startGet)
-        self.doGet(settings);
+            $.ajax({
+                dataType: "json",
+                url: serviceUrl,
+                data: { ids: id_collection },
+                traditional: true,
+                success: function(data) { $(data).each(function(i, item) { self.doClassFav(item); }); },
+                error: self.ajaxerror
+            });
+        }
 
-   return self;
-};
+        self.onClick = function($target) {
+            var serviceUrl = settings.serviceUrlPost;
+            var id = $target.data(self.getSelectorName());
 
-})( jQuery );
+            if ($target.hasClass(settings.classFav))
+                serviceUrl = settings.serviceUrlPostRemove || settings.serviceUrlPost;
+
+            serviceUrl = serviceUrl + '?id=' + id;
+            $.post(serviceUrl, { id: id },
+                function(data) {
+                    $(data).each(function(i, item) {
+                        self.doOnAuthenticated(item, function() {
+                            self.doClassFav(item);
+                        });
+                    });
+                }, 'json').fail(self.ajaxerror);
+        }
+
+        /*## INIT ## */
+        $(document).on('click', settings.selector, function() { self.onClick($(this)); });
+
+        if (settings.startGet)
+            self.doGet(settings);
+
+        return self;
+    };
+
+})(jQuery);
---
+++ src/microsoft/helpme/HelpMe/Service/LogService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace HelpMe.Service
+{
+    public static class LogService
+    {
+        static string LogPath
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["MyQuery.LogPath"] as string;
+            }
+        }
+
+        public static void Write(string contents)
+        {
+            File.WriteAllText(string.Concat(LogPath, DateTime.Now.ToString("dd_MM_yyyy"), ".log"), contents);
+        }
+    }
+}
